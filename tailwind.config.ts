fb4bfefd7056e25f7e77090743a4a5ebb3dc0e93
Jason <jason25840@gmail.com>
2024-11-09 07:57:00 -0500

    configure Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,13 @@
 import type { Config } from "tailwindcss";
 
+import typography from "@tailwindcss/typography";
+import forms from "@tailwindcss/forms";
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import containerQueries from "@tailwindcss/container-queries";
+import animate from "tailwindcss-animate";
+import radix from "tailwindcss-radix";
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +22,14 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    typography,
+    forms,
+    aspectRatio,
+    containerQueries,
+    animate,
+    radix,
+    daisyui,
+
+  ],
 } satisfies Config;
