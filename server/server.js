05d3918956c0f146d5f71b747c735c2f65de44db
Jason <jason25840@gmail.com>
2024-12-13 05:55:07 -0500

    install backend packages and configure

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Config
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Basic Route
+app.get('/', (req, res) => res.send('Backend API Running'));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error(err));
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
